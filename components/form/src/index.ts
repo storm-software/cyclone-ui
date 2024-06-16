12adc90d4a9a0517fe3bcdc4df74ebeb71ee8e4f
Patrick Sullivan <patrick.joseph.sullivan@protonmail.com>
2024-06-16 03:55:51 +0100

    feat(form): Initial check-in of Form component library

---
+++ components/form/src/index.ts
@@ -0,0 +1,10 @@
+/**
+ * The form library used by Storm Software for building TypeScript applications.
+ *
+ * @remarks
+ * A Form management package used to handle user input in Storm Software applications
+ *
+ * @packageDocumentation
+ */
+
+export const variable = "form";
